76b8c3726f5286f6d5423a8505903632a9e9f7db
MonroeLionel <MonroeLionel@gmail.com>
2025-11-06 16:40:47 +0300

    feat: add extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,9 +16,27 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function repeater(/* str, options */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  const opt = {
+    repeatTimes: options.repeatTimes || 1,
+    separator: options.separator || '+',
+    addition: options.hasOwnProperty('addition') ? String(options.addition) : '',
+    additionRepeatTimes: options.additionRepeatTimes || 1,
+    additionSeparator: options.additionSeparator || '|'
+  }
+  let addition = ''
+  for (let i = 0; i < opt.additionRepeatTimes; i++) {
+    addition = addition + `${opt.addition}${opt.additionSeparator}`
+  }
+  addition = addition.slice(0, addition.length - opt.additionSeparator.length)
+  console.log(opt.additionSeparator.length)
+  let result = ''
+  for (let i = 0; i < opt.repeatTimes; i++) {
+    console.log(opt.repeatTimes)
+    result = result + `${str}${addition}${opt.separator}`
+  }
+  result = result.slice(0, result.length - opt.separator.length)
+  return result
 }
 
 module.exports = {
